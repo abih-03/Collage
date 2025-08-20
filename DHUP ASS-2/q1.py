347213cda963fc7870fdcdbcbeef2f4119654797
abih-03 <abhisavani8@gmail.com>
2025-08-20 13:21:59 +0530

    Delete DHUP ASS-2 directory

--- DHUP ASS-2/q1.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-try:
-    conn = sqlite3.connect("Student_Information.db")
-    print("Database created and connected successfully.")
-except Exception as e:
-    print("Error creating database:", e)
-finally:
-    conn.close()
